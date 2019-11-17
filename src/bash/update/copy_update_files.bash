34b5efac6c73d2a87a0daf78e6b2d07ec5507f23
Dong Nguyen <nvdkg1999@gmail.com>
2019-11-17 21:35:21 +0700

    Fix cannot update welcome message

--- src/bash/update/copy_update_files.bash
+++ src/bash/update/copy_update_files.bash
@@ -1,9 +1,9 @@
 #require download update file success
 update_path="$DNT_HOME/update"
 if [ -d "$update_path/dntcmd" ]; then
-    "$update_path/dntcmd/setup"
     sudo rm -r $DNT_HOME/bash $DNT_HOME/cmd $DNT_HOME/py $DNT_HOME/help 
-    # cd "$update_path/dntcmd/src"
-    # sudo cp -r -t $DNT_HOME `ls`
+    cd "$update_path/dntcmd/src"
+    sudo cp -r -t $DNT_HOME `ls`
     sudo rm -r "$update_path/dntcmd"
+    dntcmd on
 fi
