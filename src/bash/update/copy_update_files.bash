6a318bb76c01aa269834a8ccf0d145c0f2179913
Dong Nguyen <nvdkg1999@gmail.com>
2019-11-17 20:58:52 +0700

    The 0.01 release

--- src/bash/update/copy_update_files.bash
+++ src/bash/update/copy_update_files.bash
@@ -1,6 +1,5 @@
 #require download update file success
 update_path="$DNT_HOME/update"
-echo "$update_path/dntcmd"
 if [ -d "$update_path/dntcmd" ]; then
     sudo rm -r $DNT_HOME/bash $DNT_HOME/cmd $DNT_HOME/py $DNT_HOME/help 
     cd "$update_path/dntcmd/src"
