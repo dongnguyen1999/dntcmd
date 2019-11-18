8533c2c1539c1d1aa6000048b34eac9db95e5d0e
Dong Nguyen <nvdkg1999@gmail.com>
2019-11-18 10:23:27 +0700

    Test new update feature

--- src/bash/update/get_update_files.bash
+++ src/bash/update/get_update_files.bash
@@ -2,7 +2,7 @@
 sudo apt install git -y
 update_path="$DNT_HOME/update"
 if [ ! -d $update_path ]; then
-    sudo mkdir -p $update_path
+    sudo mkdir -p $update_path > "/var/dntcmd/getupdate.log"
 fi
 cd $update_path
-sudo git clone $UPDATE_REPO
+sudo git clone $UPDATE_REPO > "/var/dntcmd/getupdate.log"
