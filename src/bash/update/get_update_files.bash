0cb9e8b1b676dc6f3a13ed69379d27ed797b1ee0
Dong Nguyen <nvdkg1999@gmail.com>
2019-11-18 11:57:28 +0700

    Fixing update feature

--- src/bash/update/get_update_files.bash
+++ src/bash/update/get_update_files.bash
@@ -1,6 +1,8 @@
 #install git
 sudo apt install git -y 1> "/var/dntcmd/getupdate.log" 2> "/var/dntcmd/getupdate.log"
 echo "Updateing..."
+DNT_HOME=`cat $home_path` 
+UPDATE_REPO="https://dongnguyen1999:Dong1357432589@github.com/dongnguyen1999/dntcmd.git"
 update_path="$DNT_HOME/update"
 echo $DNT_HOME
 if [ ! -d $update_path ]; then
@@ -8,4 +10,4 @@ if [ ! -d $update_path ]; then
 fi
 cd $update_path
 echo "$UPDATE_REPO"
-sudo git clone $UPDATE_REPO #1> "/var/dntcmd/getupdate.log" 2> "/var/dntcmd/getupdate.log"
+sudo git clone $UPDATE_REPO 1> "/var/dntcmd/getupdate.log" 2> "/var/dntcmd/getupdate.log"
