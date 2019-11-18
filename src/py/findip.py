fe41479bf846b33dc0292053431664fb92fec780
Dong Nguyen <nvdkg1999@gmail.com>
2019-11-18 15:13:41 +0700

    Add findip cmd

---
+++ src/py/findip.py
@@ -0,0 +1,21 @@
+import sys
+import subprocess
+
+if len(sys.argv) > 1:
+    arg1 = sys.argv[1]
+    if (arg1.lower() == "dong"
+        or arg1.lower() == "ndong"
+        or arg1.lower() == "anh"
+        or arg1.lower() == "a"
+        arg1.lower() == "adev"
+        ):
+        print("25.77.168.129")
+    elif (arg1.lower() == "thu"
+        or arg1.lower() == "mthu"
+        or arg1.lower() == "em"
+        or arg1.lower() == "e"
+        arg1.lower() == "minhthu"
+        ):
+        print("25.77.159.103")
+else:
+    subprocess.call("ifconfig", shell=True)
