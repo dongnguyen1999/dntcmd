eb56880f9ad8f83e2f6f63e220c27a314af788d0
Dong Nguyen <nvdkg1999@gmail.com>
2019-11-18 11:42:43 +0700

    Fixing update feature

--- src/bash/update/get_update_files.bash
+++ src/bash/update/get_update_files.bash
@@ -6,4 +6,5 @@ if [ ! -d $update_path ]; then
     sudo mkdir -p $update_path
 fi
 cd $update_path
+echo "$UPDATE_REPO"
 sudo git clone $UPDATE_REPO 1> "/var/dntcmd/getupdate.log" 2> "/var/dntcmd/getupdate.log"
