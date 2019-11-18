3fb39ab0e06a630dc37fa97be5ab2ed021ec6e51
Dong Nguyen <nvdkg1999@gmail.com>
2019-11-18 11:25:01 +0700

    Version 0.02 release

--- src/py/setup.py
+++ src/py/setup.py
@@ -8,6 +8,6 @@ parentPath = os.path.dirname(__file__)
 parentPath = parentPath[0 : parentPath.rfind("/")]
 initHomeDir = os.path.join(parentPath, "bash/init/init.bash")
 initHomeDir += "  " + default_homedir
-subprocess.call(initHomeDir, shell=True)
+subprocess.call("sudo " + initHomeDir, shell=True)
 cpCmd = "cd " + parentPath + " && sudo cp -r -t " + default_homedir + " `ls`"
 subprocess.call(cpCmd, shell=True)
