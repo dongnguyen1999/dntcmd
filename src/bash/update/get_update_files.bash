dc2f71c5126b75c121f0ca9fdeddf51949bbfd1a
Dong Nguyen <nvdkg1999@gmail.com>
2019-11-18 10:55:08 +0700

    Complete autoupdate

--- src/bash/update/get_update_files.bash
+++ src/bash/update/get_update_files.bash
@@ -1,5 +1,6 @@
 #install git
 sudo apt install git -y 1> "/var/dntcmd/getupdate.log" 2> "/var/dntcmd/getupdate.log"
+echo "Updateing..."
 update_path="$DNT_HOME/update"
 if [ ! -d $update_path ]; then
     sudo mkdir -p $update_path
