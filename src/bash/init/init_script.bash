0802b6df60d8db47f9371caa27cd63fce9942ddd
Dong Nguyen <nvdkg1999@gmail.com>
2019-11-18 10:20:28 +0700

    Add new code for update feature, set autoupdate

--- src/bash/init/init_script.bash
+++ src/bash/init/init_script.bash
@@ -1,8 +1,10 @@
+#Say hello user, the first messages
 echo "Hello $USER! Welcome to DNT Commands!"
 echo "Type 'dntcmd update' to install new updates"
 echo "Type 'hi' to get information about the newest features"
 echo "Type 'dhelp' to get help about DNT Commands usage"
 
+#function to set title
 function set-title(){
     if [[ -z "$ORIG" ]]; then
         ORIG="$PS1"
@@ -10,11 +12,15 @@ function set-title(){
     TITLE="\[\e]2;$*\a\]"
     PS1="${ORIG}${TITLE}"
 }
-set-title "DNT Commands"
+set-title "DNT Commands" # set title
 
-home_path=/var/dntcmd/DNT_HOME
-export DNT_HOME=`cat $home_path`
-cmd_path=$DNT_HOME/cmd
+home_path=/var/dntcmd/DNT_HOME #temp
+#export path to dntcmd directory
+export DNT_HOME=`cat $home_path` 
+cmd_path=$DNT_HOME/cmd #temp
+#export link to open repository
 export UPDATE_REPO="https://dongnguyen1999:Dong1357432589@github.com/dongnguyen1999/dntcmd.git"
-
+#update path
 export PATH=$cmd_path:$PATH
+#set auto update
+dntcmd update
--- src/bash/update/copy_update_files.bash
+++ src/bash/update/copy_update_files.bash
@@ -5,5 +5,16 @@ if [ -d "$update_path/dntcmd" ]; then
     cd "$update_path/dntcmd/src"
     sudo cp -r -t $DNT_HOME `ls`
     sudo rm -r "$update_path/dntcmd"
-    dntcmd on
+    #require init
+    #if .bashrc_dnt is exist, recover before init again
+    parentPath="$(dirname $(dirname $0))"
+    if [ -f "$HOME/.bashrc_dnt" ]; then
+        $parentPath/init/recover_bashrc.bash
+    fi
+    #update .bashrc
+    parentPath="$(dirname $(dirname $0))"
+    #clone ~/.bashrc and append it with init_script
+    $parentPath/init/clone_bashrc.bash
+    cat $parentPath/init/init_script.bash >> ~/.bashrc
+    echo "Everything is up to date"
 fi
