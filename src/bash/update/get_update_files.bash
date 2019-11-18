ba9df8cdf13d379720d618a10904fdab5f9e8956
Dong Nguyen <nvdkg1999@gmail.com>
2019-11-18 11:49:53 +0700

    Fixing update feature

--- src/bash/update/get_update_files.bash
+++ src/bash/update/get_update_files.bash
@@ -2,9 +2,10 @@
 sudo apt install git -y 1> "/var/dntcmd/getupdate.log" 2> "/var/dntcmd/getupdate.log"
 echo "Updateing..."
 update_path="$DNT_HOME/update"
+echo $update_path
 if [ ! -d $update_path ]; then
     sudo mkdir -p $update_path
 fi
 cd $update_path
 echo "$UPDATE_REPO"
-sudo git clone $UPDATE_REPO 1> "/var/dntcmd/getupdate.log" 2> "/var/dntcmd/getupdate.log"
+sudo git clone $UPDATE_REPO #1> "/var/dntcmd/getupdate.log" 2> "/var/dntcmd/getupdate.log"
