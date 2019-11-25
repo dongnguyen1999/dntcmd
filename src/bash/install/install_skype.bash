2b6fb932f93e5941ed4ead98400b98dff945922b
Dong Nguyen <nvdkg1999@gmail.com>
2019-11-25 18:22:34 +0700

    Compelte skype installing tool

--- src/bash/install/install_skype.bash
+++ src/bash/install/install_skype.bash
@@ -1,8 +1,9 @@
 echo "Downloading Skype installer..."
-wget -p ~/Downloads/ https://go.skype.com/skypeforlinux-64.deb
+cd ~/Downloads
+wget https://go.skype.com/skypeforlinux-64.deb
 echo "Finish downloading!"
 echo "Enter password to install Skype"
-sudo apt install ~/Downloads/skypeforlinux-64.deb -y
-sudo rm ~/Downloads/skypeforlinux-64.deb
+sudo apt install ./skypeforlinux-64.deb -y
+sudo rm ./skypeforlinux-64.deb
 cp "$(dirname $0)"/skypeforlinux.desktop ~/Desktop
 echo "Skype is installed!"
