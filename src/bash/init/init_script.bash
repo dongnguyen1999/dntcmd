4173872c8abc2b71cdcb30ccb7622d57f0da7fa8
Dong Nguyen <nvdkg1999@gmail.com>
2019-12-13 19:09:38 +0700

    Add new string for bash cursor name

--- src/bash/init/init_script.bash
+++ src/bash/init/init_script.bash
@@ -3,6 +3,7 @@ echo "Hello $USER! Welcome to ĐNT Commands ver 0.11!"
 echo "Type 'hi' to get information about the newest features"
 echo "Type 'dhelp' to get help about ĐNT Commands usage"
 
+export PS1="\[\e[32m\]$USER@DNT-Commands:\w$\[\e[m\]"
 #function to set title
 function set-title(){
     if [[ -z "$ORIG" ]]; then
@@ -12,8 +13,6 @@ function set-title(){
     PS1="${ORIG}${TITLE}"
 }
 set-title "ĐNT Commands" # set title
-
-export PS1="$USER@DNT-Commands\w$"
 home_path=/var/dntcmd/DNT_HOME #temp
 #export path to dntcmd directory
 export DNT_HOME=`cat $home_path` 
