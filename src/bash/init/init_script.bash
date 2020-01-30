8d70c2088d7bb080232750fd732096a46d608997
Dong Nguyen <nvdkg1999@gmail.com>
2020-01-30 10:22:17 +0700

    Fix title bug

--- src/bash/init/init_script.bash
+++ src/bash/init/init_script.bash
@@ -4,7 +4,7 @@ echo -e "\e[1;32mHello \e[1;34m$USER\e[1;32m! Welcome to \e[1;33mĐNT Commands v
 echo -e "\e[1;32mType \e[1;31m'hi'\e[1;32m to get information about the newest features"
 echo -e "\e[1;32mType \e[1;31m'dhelp'\e[1;32m to get help about ĐNT Commands usage"
 
-export PS1="\[\033[01;32m\]$USER@ĐNT-Commands\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$\[\033[01;33m\]$*[\033[00m\]"
+export PS1="\[\033[01;32m\]$USER@ĐNT-Commands\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$\[\033[01;33m\]"
 #function to set title
 function set-title(){
     if [[ -z "$ORIG" ]]; then
