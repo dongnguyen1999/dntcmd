f557bf1644ded5447b90d48556b2fef792b5eaa2
Dong Nguyen <nvdkg1999@gmail.com>
2020-01-30 09:31:46 +0700

    Color hi

--- src/bash/init/init_script.bash
+++ src/bash/init/init_script.bash
@@ -10,7 +10,7 @@ function set-title(){
     if [[ -z "$ORIG" ]]; then
         ORIG="$PS1"
     fi
-    TITLE="\[\e]2;$*\a\]"
+    TITLE="\[\e]2;\e[1;32$*\a\]"
     PS1="${ORIG}${TITLE}"
 }
 set-title "ĐNT Commands" # set title
