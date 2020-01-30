573b753b1fd5b21635f62b3282b0df9914f596f6
Dong Nguyen <nvdkg1999@gmail.com>
2020-01-30 10:23:45 +0700

    Fix title bug

--- src/bash/init/init_script.bash
+++ src/bash/init/init_script.bash
@@ -10,7 +10,7 @@ function set-title(){
     if [[ -z "$ORIG" ]]; then
         ORIG="$PS1"
     fi
-    TITLE="\[\e]2;\[\033[01;32m\]$*\[\033[00m\]\a\]"
+    TITLE="\[\e]2;$*\a\]"
     PS1="${ORIG}${TITLE}"
 }
 set-title "ĐNT Commands" # set title
