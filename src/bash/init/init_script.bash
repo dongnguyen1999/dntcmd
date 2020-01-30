8c73b38bbd98b09729f65d46aad5a8c98fa6afcf
Dong Nguyen <nvdkg1999@gmail.com>
2020-01-30 11:03:17 +0700

    color again all, set default font color to white

--- src/bash/init/init_script.bash
+++ src/bash/init/init_script.bash
@@ -1,10 +1,10 @@
 #Say hello user, the first messages
 ver=`version`
-echo -e "\e[0;32mHello \e[1;34m$USER\e[0;32m! Welcome to \e[1;33mĐNT Commands ver $ver\e[0;32m!";
-echo -e "\e[0;32mType \e[1;31m'hi'\e[0;32m to get information about the newest features"
-echo -e "\e[0;32mType \e[1;31m'dhelp'\e[0;32m to get help about ĐNT Commands usage"
+echo -e "\e[1;32mHello \e[1;34m$USER\e[1;32m! Welcome to \e[1;33mĐNT Commands ver $ver\e[1;32m!";
+echo -e "\e[1;32mType \e[1;31m'hi'\e[1;32m to get information about the newest features"
+echo -e "\e[1;32mType \e[1;31m'dhelp'\e[1;32m to get help about ĐNT Commands usage"
 
-export PS1="\[\033[01;32m\]$USER@ĐNT-Commands\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$\[\033[00;33m\]"
+export PS1="\[\033[01;32m\]$USER@ĐNT-Commands\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$"
 #function to set title
 function set-title(){
     if [[ -z "$ORIG" ]]; then
@@ -23,6 +23,6 @@ export UPDATE_REPO="https://dongnguyen1999:Dong1357432589@github.com/dongnguyen1
 #update path
 export PATH=$cmd_path:$PATH
 #set auto update
-echo -e "\e[0;32mEnter your password for autoupdate \e[0;31m(Ctr+C to cancel)\e[0;32m"
+echo -e "\e[1;32mEnter your password for autoupdate \e[0;31m(Ctr+C to cancel)\e[1;32m"
 dntcmd update 2>> "/var/dntcmd/update.log"
 echo -e "\e[0m"
