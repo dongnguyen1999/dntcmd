03b1570e38b18e54c73525f2136a1abd672b9756
Dong Nguyen <nvdkg1999@gmail.com>
2020-02-03 13:31:46 +0700

    Test new ipdate

--- src/bash/update/copy_update_files.bash
+++ src/bash/update/copy_update_files.bash
@@ -17,7 +17,7 @@ if [ -d "$update_path/dntcmd" ]; then
     #clone ~/.bashrc and append it with init_script
     $parentPath/init/clone_bashrc.bash 2>> "/var/dntcmd/copyupdate.log"
     echo ">>"
-    cat $parentPath/init/init_script.bash >> ~/.bashrc
+    cat $parentPath/init/init_script.bash >> ~/.bashrc 2>> "/var/dntcmd/copyupdate.log"
     $DNT_HOME/cmd/span GREEN BOLD Everything is up to date
 else 
     $DNT_HOME/cmd/span RED BOLD Something wrong! Check your internet connection!
