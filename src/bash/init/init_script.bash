98651b33ec1e47abb80d4e51e0dd3995b44cf5c8
Dong Nguyen <nvdkg1999@gmail.com>
2020-02-03 08:43:25 +0700

    Test export values 2

--- src/bash/init/init_script.bash
+++ src/bash/init/init_script.bash
@@ -27,5 +27,4 @@ echo -e "\e[1;32mEnter your password for autoupdate \e[1;31m(Ctr+C to cancel)\e[
 dntcmd update 2>> "/var/dntcmd/update.log"
 echo -e "\e[0m"
 #export constances
-parentPath="$(dirname $0)"
-$parentPath/export_values.bash 2>> "/var/dntcmd/error.log"
+$DNT_HOME/bash/init/export_values.bash 2>> "/var/dntcmd/error.log"
