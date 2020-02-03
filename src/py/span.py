6de26b839950f3be799591ac8856ed5f918f1c1e
Dong Nguyen <nvdkg1999@gmail.com>
2020-02-03 16:30:30 +0700

    Try to fix lag

--- src/py/span.py
+++ src/py/span.py
@@ -83,17 +83,55 @@ def printWhitespaces():
         string += "\t" + char + "\n"
     subprocess.call("echo '" + string + "'", shell=True)
 
-tagnames = {}
-def joinTagnames():
-    for key, value in colors.items():
-        tagnames[key] = value
-    for key, value in backgrounds.items():
-        tagnames[key] = value
-    for key, value in styles.items():
-        tagnames[key] = value
-    for key, value in whitespaces.items():
-        tagnames[key] = value
-joinTagnames()
+tagnames = {
+    "DEFAULT-COLOR": "\e[0;39m",
+    "BLACK": "\e[0;30m",
+    "GREEN": "\e[0;32m",
+    "RED": "\e[0;31m",
+    "YELLOW": "\e[0;33m",
+    "BLUE": "\e[0;34m",
+    "MAGENTA": "\e[0;35m",
+    "CYAN": "\e[0;36m",
+    "LIGHT-GRAY": "\e[0;37m",
+    "DARK-GRAY": "\e[0;90m",
+    "LIGHT-RED": "\e[0;91m",
+    "LIGHT-GREEN": "\e[0;92m",
+    "LIGHT-YELLOW": "\e[0;93m",
+    "LIGHT-BLUE": "\e[0;94m",
+    "LIGHT-MAGENTA": "\e[0;95m",
+    "LIGHT-CYAN": "\e[0;96m",
+    "WHITE": "\e[0;97m",
+
+    "DEFAULT-COLOR-BG": "\e[49m",
+    "BLACK-BG": "\e[40m",
+    "GREEN-BG": "\e[42m",
+    "RED-BG": "\e[41m",
+    "YELLOW-BG": "\e[43m",
+    "BLUE-BG": "\e[44m",
+    "MAGENTA-BG": "\e[45m",
+    "CYAN-BG": "\e[46m",
+    "LIGHT-GRAY-BG": "\e[47m",
+    "DARK-GRAY-BG": "\e[100m",
+    "LIGHT-RED-BG": "\e[101m",
+    "LIGHT-GREEN-BG": "\e[102m",
+    "LIGHT-YELLOW-BG": "\e[103m",
+    "LIGHT-BLUE-BG": "\e[104m",
+    "LIGHT-MAGENTA-BG": "\e[105m",
+    "LIGHT-CYAN-BG": "\e[106m",
+    "WHITE-BG": "\e[107m",
+
+    "DEFAULT": "\e[0m",
+    "BOLD": "\e[1m",
+    "DIM": "\e[2m",
+    "UNDERLINED": "\e[4m",
+    "BLINK": "\e[5m",
+    "INVERTED": "\e[7m",
+    "HIDDEN": "\e[8m",
+
+    "TAB": "    ",
+    "NEWLINE": "\n",
+    "INLINE": ""
+}
 
 closeTag = styles.get("DEFAULT")
 
