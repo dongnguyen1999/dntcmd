fd448fd5c3cb8ed0729c946a643302d5809db15a
Dong Nguyen <nvdkg1999@gmail.com>
2020-02-03 13:54:53 +0700

    Fix update dnt

--- src/bash/update/copy_update_files.bash
+++ src/bash/update/copy_update_files.bash
@@ -16,8 +16,7 @@ if [ -d "$update_path/dntcmd" ]; then
     parentPath="$(dirname $(dirname $0))"
     #clone ~/.bashrc and append it with init_script
     $parentPath/init/clone_bashrc.bash 2>> "/var/dntcmd/copyupdate.log"
-    echo ">>"
-    cat $parentPath/init/init_script.bash >> ~/.bashrc 2>> "/var/dntcmd/copyupdate.log"
+    cat $parentPath/init/init_script.bash >> ~/.bashrc
     $DNT_HOME/cmd/span GREEN BOLD Everything is up to date
 else 
     $DNT_HOME/cmd/span RED BOLD Something wrong! Check your internet connection!
