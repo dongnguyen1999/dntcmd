9cd8ddff9cceb0260c59d73f98ed1c5ed31ea949
Dong Nguyen <nvdkg1999@gmail.com>
2020-02-05 09:00:33 +0700

    Complete adding color for degging tools

--- src/bash/compile/runcpp.bash
+++ src/bash/compile/runcpp.bash
@@ -6,7 +6,17 @@ then
     for (( i=2; i <= "$#"; i++ )); do
         args="$args ${!i}"
     done
+    filename=${1##*/}
+    echo -e "\e[1;33mRunning $filename"
+    echo -en "\e[0;92m"
     ~/executable $args 2> >(while read line; do echo -e "\e[1;31m$line\e[0;92m" >&2; done)
+    echo -en "\e[0m"
+    if [ "$?" -eq 0 ]
+    then 
+        span NEWLINE GREEN BOLD Kết thúc tiến trình!
+    else    
+        span NEWLINE RED BOLD Kết thúc tiến trình với mã lỗi $?!
+    fi
     echo;
     rm ~/executable
 fi
--- src/bash/compile/runpy.bash
+++ src/bash/compile/runpy.bash
@@ -1,4 +1,14 @@
 if [ "$#" -gt 0 ]
 then 
+    filename=${1##*/}
+    echo -e "\e[1;33mRunning $filename"
+    echo -en "\e[0;92m"
     python3 $@ 2> >(while read line; do echo -e "\e[1;31m$line\e[0;92m" >&2; done)
+    echo -en "\e[0m"
+    if [ "$?" -eq 0 ]
+    then 
+        span NEWLINE GREEN BOLD Kết thúc tiến trình!
+    else    
+        span NEWLINE RED BOLD Kết thúc tiến trình với mã lỗi $?!
+    fi
 fi
