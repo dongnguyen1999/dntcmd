78ed41ba0662465018a34c57c0fbf97bcd57b288
Dong Nguyen <nvdkg1999@gmail.com>
2020-03-18 16:32:54 +0700

    Support python2 for runpy

--- src/bash/compile/runpy.bash
+++ src/bash/compile/runpy.bash
@@ -1,10 +1,18 @@
 if [ "$#" -gt 0 ]
 then 
     filename=${1##*/}
-    echo -e "\e[1;33mRunning $filename"
-    echo -en "\e[0;92m"
-    python3 $@ 2> >(while read line; do echo -e "\e[1;31m$line\e[0;92m" >&2; done)
+    if [ "$#" -gt 1  -a  "$2" == "py2" ]
+    then
+        echo -e "\e[1;33mRunning $filename with python2"
+        echo -en "\e[0;92m"
+        python $@ 2> >(while read line; do echo -e "\e[1;31m$line\e[0;92m" >&2; done)
+    else
+        echo -e "\e[1;33mRunning $filename with python3"
+        echo -en "\e[0;92m"
+        python3 $@ 2> >(while read line; do echo -e "\e[1;31m$line\e[0;92m" >&2; done)
+    fi
     echo -en "\e[0m"
+
     if [ "$?" -eq 0 ]
     then 
         span NEWLINE GREEN BOLD Kết thúc tiến trình!
