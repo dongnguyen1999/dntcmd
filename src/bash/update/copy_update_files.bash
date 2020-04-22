f49a2ecbfdb46a8145d91c6a5f892eb2e821e5df
Dong Nguyen <nvdkg1999@gmail.com.com>
2020-04-22 09:33:22 +0700

    Test service system

--- src/bash/update/copy_update_files.bash
+++ src/bash/update/copy_update_files.bash
@@ -2,7 +2,7 @@
 DNT_HOME=`cat /var/dntcmd/DNT_HOME` 
 update_path="$DNT_HOME/update"
 if [ -d "$update_path/dntcmd" ]; then
-    sudo rm -r $DNT_HOME/bash $DNT_HOME/cmd $DNT_HOME/py $DNT_HOME/help $DNT_HOME/cpp $DNT_HOME/service
+    sudo rm -r $DNT_HOME/bash $DNT_HOME/cmd $DNT_HOME/py $DNT_HOME/help $DNT_HOME/cpp $DNT_HOME/service $DNT_HOME/jar
     cd "$update_path/dntcmd/src"
     sudo cp -r -t $DNT_HOME `ls`
     sudo rm -r "$update_path/dntcmd"
