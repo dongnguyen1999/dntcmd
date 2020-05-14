1007c4ad90d6de4a7f65e7147b55e3b871aed024
Dong Nguyen <nvdkg1999@gmail.com.com>
2020-05-14 21:18:17 +0700

    Change desktop icon permission

--- src/bash/install/kazam.bash
+++ src/bash/install/kazam.bash
@@ -2,4 +2,5 @@ echo "\e[0;33mInstalling kazam video recorder..."
 sudo apt install -y kazam
 sudo cp "$(dirname $0)"/kazam_icon.png /usr/share/pixmaps
 cp "$(dirname $0)"/kazam.desktop ~/Desktop
+chown $USER:$USER ~/Desktop/kazam.desktop
 echo "\e[0;32mKazam is installed!"
